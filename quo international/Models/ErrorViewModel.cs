d83dbb057e1b063b7f1a1f2ab03a3200ff757986
HP <HP@DESKTOP-S9SE3B3>
2024-05-28 22:26:14 -0700

    Add project files.

---
+++ quo international/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace quo_international.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
